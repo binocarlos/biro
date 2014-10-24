bf319f8c1797899391ddc8cc905b6e384c59060b
Kai Davenport <kaiyadavenport@gmail.com>
2014-10-24 13:36:39 +0000

    update example

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
-var fs = require('fs')
 var angular = require('angular-bsfy')
-
-var templates = {
-  
-}
 
 module.exports = angular
 	.module('biro', [
     
   ])
 
-  .directive('biroForm', function(){
-
-    function controller($scope){
-      $scope.layout = $scope.layout || 'basic'
-      $scope.readonly = $scope.readonly=='true' ? true : false
-    }
-
-    function linker($scope, $elem, $attr) {
-      
-    }
-
-    return {
-      restrict:'EA',
-      scope:{
-        schema:'=',
-        model:'=',
-        readonly:'@',
-        layout:'@'
-      },
-      replace:true,
-      controller:controller,
-      link:linker
-    }
-  })
+  .directive('biroForm', require('./form'))
+  .directive('biroField', require('./field'))
